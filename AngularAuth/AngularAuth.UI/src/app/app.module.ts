c1eef7969f1a18973124518e4dbf105f633d9094
Drackus001 <Satyam.Agarwal.IN@gmail.com>
2023-08-31 18:07:27 +0530

    login html and reactive form added

--- AngularAuth/AngularAuth.UI/src/app/app.module.ts
+++ AngularAuth/AngularAuth.UI/src/app/app.module.ts
@@ -3,20 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ReactiveFormsModule } from '@angular/forms';
+
 import { LoginComponent } from './components/login/login.component';
 import { SignupComponent } from './components/signup/signup.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    LoginComponent,
-    SignupComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, LoginComponent, SignupComponent],
+  imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- AngularAuth/AngularAuth.UI/src/app/components/login/login.component.ts
+++ AngularAuth/AngularAuth.UI/src/app/components/login/login.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormGroup, FormControl, FormArray, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  public loginForm!: FormGroup;
 
+  ngOnInit() {
+    this.initForm();
+  }
+
+  private initForm() {
+    this.loginForm = new FormGroup({
+      email: new FormControl(null, Validators.required),
+      password: new FormControl(null, Validators.required),
+    });
+  }
+  onSubmit(): void {
+    console.log(this.loginForm);
+  }
 }
