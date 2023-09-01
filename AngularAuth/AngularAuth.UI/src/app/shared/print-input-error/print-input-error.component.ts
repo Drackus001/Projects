ace319950b20e9bdc6917f6aaecf849a3a5b9cfe
Drackus001 <Satyam.Agarwal.IN@gmail.com>
2023-09-01 20:38:17 +0530

    shared print error component added

--- AngularAuth/AngularAuth.UI/src/app/shared/print-input-error/print-input-error.component.ts
+++ AngularAuth/AngularAuth.UI/src/app/shared/print-input-error/print-input-error.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
-  selector: 'app-print-input-error',
+  selector: 'print-input-error',
   templateUrl: './print-input-error.component.html',
-  styleUrls: ['./print-input-error.component.scss']
+  styleUrls: ['./print-input-error.component.scss'],
 })
 export class PrintInputErrorComponent {
-
+  @Input('control') control: any;
 }
