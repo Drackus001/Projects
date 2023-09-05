6bbc4b556666f63ae1978790652ae7b1a859fc00
Drackus001 <Satyam.Agarwal.IN@gmail.com>
2023-09-05 10:24:12 +0530

    form builder added

--- AngularAuth/AngularAuth.UI/src/app/components/login/login.component.ts
+++ AngularAuth/AngularAuth.UI/src/app/components/login/login.component.ts
@@ -1,26 +1,38 @@
 import { Component, OnInit } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
-import { FormGroup, FormControl, FormArray, Validators } from '@angular/forms';
+import { FormBuilder } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
 })
-export class LoginComponent implements OnInit {
-  public loginForm!: FormGroup;
+export class LoginComponent {
+  public loginForm = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', Validators.required],
+  });
 
-  ngOnInit() {
-    this.initForm();
-  }
+  constructor(private fb: FormBuilder) {}
 
-  private initForm() {
-    this.loginForm = new FormGroup({
-      email: new FormControl(null, Validators.required),
-      password: new FormControl(null, Validators.required),
-    });
-  }
   onSubmit(): void {
-    console.log(this.loginForm);
+    if (this.loginForm.valid) {
+      // send data to api calls
+      console.log(this.loginForm.value);
+    } else {
+      this.validateAllFormFields(this.loginForm);
+      alert('Form is InValid!');
+    }
+  }
+
+  private validateAllFormFields(formGroup: FormGroup): void {
+    Object.keys(formGroup.controls).forEach((field) => {
+      const control = formGroup.get(field);
+      if (control instanceof FormControl) {
+        control.markAsDirty({ onlySelf: true });
+      } else if (control instanceof FormGroup) {
+        this.validateAllFormFields(control);
+      }
+    });
   }
 }
