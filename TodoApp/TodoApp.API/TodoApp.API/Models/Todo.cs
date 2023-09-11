7395709df75eec9f92379f1800ea84442b550d25
Drackus001 <Satyam.Agarwal.IN@gmail.com>
2023-09-11 11:45:52 +0530

    Todo model added

---
+++ TodoApp/TodoApp.API/TodoApp.API/Models/Todo.cs
@@ -0,0 +1,11 @@
+﻿namespace TodoApp.API.Models
+{
+    public class Todo
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public bool IsCompleted { get; set; }
+        public DateTime CompletedAt { get; set; }
+    }
+}
