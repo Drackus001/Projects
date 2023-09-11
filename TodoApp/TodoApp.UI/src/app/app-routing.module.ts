84f8e964314c19f6b589b9d9e8401bd5af30a103
Drackus001 <Satyam.Agarwal.IN@gmail.com>
2023-09-11 13:35:34 +0530

    sample navbad & todos component added

--- TodoApp/TodoApp.UI/src/app/app-routing.module.ts
+++ TodoApp/TodoApp.UI/src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TodosComponent } from './components/todos/todos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: TodosComponent,
+  },
+  {
+    path: 'todos',
+    component: TodosComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
