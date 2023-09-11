126102fd2a30434de462c42fdefc5a4713213aff
Drackus001 <Satyam.Agarwal.IN@gmail.com>
2023-09-11 13:48:08 +0530

    sample todos display code added

--- TodoApp/TodoApp.UI/src/app/components/todos/todos.component.ts
+++ TodoApp/TodoApp.UI/src/app/components/todos/todos.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { Todo } from 'src/app/models/todo.models';
 
 @Component({
   selector: 'app-todos',
   templateUrl: './todos.component.html',
-  styleUrls: ['./todos.component.scss']
+  styleUrls: ['./todos.component.scss'],
 })
 export class TodosComponent {
-
+  todos: Todo[] = [];
 }
