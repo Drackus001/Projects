656eeb24436a2127e5d456e6acd3ba09787cfde9
Drackus001 <Satyam.Agarwal.IN@gmail.com>
2023-09-11 11:46:15 +0530

    AppDbContext added

---
+++ TodoApp/TodoApp.API/TodoApp.API/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TodoApp.API.Models;
+
+namespace TodoApp.API.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Todo> Todos { get; set; }
+    }
+}
