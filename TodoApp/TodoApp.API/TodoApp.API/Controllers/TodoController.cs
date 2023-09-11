f68500a397c0ae0daf62bf43692bbdd6283e7889
Drackus001 <Satyam.Agarwal.IN@gmail.com>
2023-09-11 11:47:02 +0530

    Todo initial controller added

---
+++ TodoApp/TodoApp.API/TodoApp.API/Controllers/TodoController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TodoApp.API.Data;
+
+namespace TodoApp.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TodoController : ControllerBase
+    {
+        private readonly AppDbContext _appDbContext;
+        public TodoController(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllTodos()
+        {
+            var todos = await _appDbContext.Todos.ToListAsync();
+            return Ok(todos);
+        }
+    }
+}
