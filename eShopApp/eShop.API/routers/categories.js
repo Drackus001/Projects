db14d2c0e57f605a22f6372b691269463b947c95
Drackus001 <Satyam.Agarwal.IN@gmail.com>
2023-09-14 17:30:17 +0530

    get single categories and put api added

--- eShopApp/eShop.API/routers/categories.js
+++ eShopApp/eShop.API/routers/categories.js
@@ -10,7 +10,20 @@ router.get(`/`, async (req, res) => {
     res.status(500).json({ success: false });
   }
 
-  res.send(categoryList);
+  res.status(200).send(categoryList);
+});
+
+router.get("/:id", async (req, res) => {
+  try {
+    let category = await Category.findById(req.params.id);
+    if (!category) {
+      res.status(404).json({ success: false, message: "Category not found!" });
+    }
+
+    res.status(200).send(category);
+  } catch (error) {
+    res.status(400).send({ error: error.message, success: false });
+  }
 });
 
 router.post("/", async (req, res) => {
@@ -26,7 +39,27 @@ router.post("/", async (req, res) => {
     if (!category) {
       res.status(400).send("The Category cannot be created.");
     }
-    res.send(category);
+    res.status(201).send(category);
+  } catch (error) {
+    res.status(400).send({ error: error.message, success: false });
+  }
+});
+
+router.put("/:id", async (req, res) => {
+  try {
+    const category = await Category.findByIdAndUpdate(
+      req.params.id,
+      {
+        name: req.body.name,
+        icon: req.body.icon,
+        color: req.body.color,
+      },
+      { new: true }
+    );
+    if (!category) {
+      res.status(404).send("The Category cannot be found.");
+    }
+    res.status(201).send({ date: category, message: "Category is updated!" });
   } catch (error) {
     res.status(400).send({ error: error.message, success: false });
   }
