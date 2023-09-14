d19662997edb6392fac1848df0609d3b5c33d53e
Drackus001 <Satyam.Agarwal.IN@gmail.com>
2023-09-14 12:58:24 +0530

    sample get & post with mongodb

--- eShopApp/eShop.API/app.js
+++ eShopApp/eShop.API/app.js
@@ -11,19 +11,48 @@ const API = process.env.API_URL;
 app.use(express.json());
 app.use(morgan("tiny"));
 
+const productSchema = mongoose.Schema({
+  name: String,
+  image: String,
+  countInStock: {
+    type: Number,
+    required: true,
+  },
+});
+
+const Product = mongoose.model("Product", productSchema);
+
 // http://localhost:3000/api/v1/products
-app.get(`${API}/products`, (req, res) => {
-  const product = {
-    id: 1,
-    name: "Laptop",
-    image: "lp.png",
-  };
-  res.send(product);
+app.get(`${API}/products`, async (req, res) => {
+  const productList = await Product.find();
+
+  if (!productList) {
+    res.status(500).json({ success: false });
+  }
+
+  res.send(productList);
 });
 
 app.post(`${API}/product`, (req, res) => {
-  const newProduct = req.body;
-  res.send(newProduct);
+  const product = new Product({
+    name: req.body.name,
+    image: req.body.image,
+    countInStock: req.body.countInStock,
+  });
+
+  product
+    .save()
+    .then((createdProduct) => {
+      res.status(201).json(createdProduct);
+    })
+    .catch((err) => {
+      res.status(500).json({
+        error: err,
+        success: false,
+      });
+    });
+
+  // res.send(newProduct);
 });
 
 mongoose
@@ -35,6 +64,6 @@ mongoose
     console.error(err);
   });
 
-app.listen(3000, () => {
+app.listen(process.env.PORT, () => {
   console.log("Server is running at http://localhost:3000");
 });
